54b7b239090ce5bdda0555f2bbbccb260901a93f
gohilnilesh <gohilnilesh44472@gmail.com>
2022-03-22 11:36:41 +0530

    update_1

--- home/MyApp/admin.py
+++ home/MyApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from MyApp.models import Contact
+
+
+
+admin.site.site_header = 'Apparal Shop'
+admin.site.index_title = 'Useful Information'
+admin.site.site_title = 'Apparel Shop'
 
 # Register your models here.
+admin.site.register(Contact)
+
+
--- home/MyApp/models.py
+++ home/MyApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField( max_length=70)
+    subject = models.TextField(max_length=30)
+    message = models.TextField(default="")
+
+    def __str__(self):
+        return self.name + "" + self.email
+    
+
+
