30755889d39170e7db24d41cde09a85c706e3a6d
gohilnilesh <gohilnilesh44472@gmail.com>
2022-04-05 16:53:33 +0530

    complete

--- home/MyApp/views.py
+++ home/MyApp/views.py
@@ -1,43 +1,5 @@
 from django.shortcuts import render
-from twilio.rest import Client
-from .forms import ContactForm
-from home import settings
-
-
-# msg = 'MESSAGE HERE'
-# account_sid = '*'
-# auth_token = '*',
-# client = Client(account_sid,auth_token)
-# message = client.messages.create(
-#     body = msg
-#     # from = 'number',
-#     # to = '+ 91 je ma moklavu 6 ema',
-# )
-
-def contact(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST or None)
-        if form.is_valid():
-            contact_name = form.cleaned_data['name']
-            contact_email = form.cleaned_data['email']
-            sub = form.cleaned_data['subject']
-            content = form.cleaned_data['message']
-            # print(contact_name)
-            form.save()
-            subject = 'Hello ' + contact_name + ' from apparel!'
-            message = 'Stay Connected. We would love to hear you!'
-            email_from = settings.EMAIL_HOST_1USER
-            email_to = [contact_email, ]
-            send_mail(subject, message, email_from, email_to)
-            messages.success(request, 'Form submitted successfully.')
-            return redirect('Home:Home')
-        else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        form = ContactForm()
-    template = 'contact.html'
-    return render(request, template, {'form': form})
-
+from django.contrib.auth.forms import UserCreationForm
 
 
 #path
@@ -73,4 +35,15 @@ def wishlist(request):
 
 def my_account(request):
     return render(request, 'my-account.html')
+
+def sign_up(request):
+
+    if request.method == "POST":
+        fm = UserCreationForm(request.POST)
+    if fm.is_valid():
+        fm.save()
+       
+    else:
+      fm = UserCreationForm()
+    return render(request,'signup.html',{'form':fm})
 
